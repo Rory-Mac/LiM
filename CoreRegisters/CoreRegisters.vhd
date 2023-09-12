89701c0bfe0bbfd2d48e8699f341e760e8fc86ae
Rory <roryobm@outlook.com>
2023-09-12 03:49:14 +0100

    create double demux on core registers to read   simultnaeously

--- CoreRegisters/CoreRegisters.vhd
+++ CoreRegisters/CoreRegisters.vhd
@@ -4,29 +4,31 @@ use work.array_package.all;
 
 entity CoreRegisters is
 port (clk, l : in std_logic;
-    sel : in std_logic_vector (0 to 4);
-    d : in std_logic_vector (0 to 31);
-    q : out std_logic_vector (0 to 31));
+    sel1, sel2 : in std_logic_vector (0 to 4);
+    d_in : in std_logic_vector (0 to 31);
+    reg_out1, reg_out2 : out std_logic_vector (0 to 31));
 end CoreRegisters;
 
 architecture CoreRegisterLogic of CoreRegisters is
     signal clk_internal, demux_in : std_logic;
-    signal sel_internal : std_logic_vector(0 to 4);
-    signal demux_out, reg_load, data_in, mux_out: std_logic_vector(0 to 31); 
+    signal sel1_internal, sel2_internal : std_logic_vector(0 to 4);
+    signal demux_out, reg_load, mux_out1, mux_out2: std_logic_vector(0 to 31); 
     signal reg_out, mux_in : vector_array(0 to 31);
 begin
-    Demux32Instance : entity work.Demux32 port map(A => demux_in, sel => sel_internal, X => demux_out);
+    Demux32Instance : entity work.Demux32 port map(A => demux_in, sel => sel1_internal, X => demux_out);
     CoreRegisters : for i in 0 to 31 generate
-        Reg32Instance : entity work.Reg32 port map (clk => clk_internal, l => reg_load(i), d => data_in, q => reg_out(i));
+        Reg32Instance : entity work.Reg32 port map (clk => clk_internal, l => reg_load(i), d => d_in, q => reg_out(i));
     end generate;
-    CoreRegMuxInstance : entity work.CoreRegMux port map(A => mux_in, sel => sel_internal, X => mux_out);
+    CoreRegMuxInstance1 : entity work.CoreRegMux port map(A => mux_in, sel => sel1_internal, X => mux_out1);
+    CoreRegMuxInstance2 : entity work.CoreRegMux port map(A => mux_in, sel => sel2_internal, X => mux_out2);
 
     clk_internal <= clk;
     demux_in <= l;
-    sel_internal <= sel;
+    sel1_internal <= sel1;
+    sel2_internal <= sel2;
     reg_load <= demux_out;
-    data_in <= d;
     mux_in <= reg_out;
-    q <= mux_out;
+    reg_out1 <= mux_out1;
+    reg_out2 <= mux_out2;
 
 end CoreRegisterLogic;
--- CoreRegisters/CoreRegistersTB.vhd
+++ CoreRegisters/CoreRegistersTB.vhd
@@ -9,10 +9,10 @@ architecture CoreRegistersTBLogic of CoreRegistersTB is
     constant CLOCK_PERIOD : time := 2 ps;
     constant SIMULATION_LENGTH : time := 1000 ps;
     signal clk, l : std_logic;
-    signal sel : std_logic_vector(0 to 4);
-    signal d, q : std_logic_vector(0 to 31);
+    signal sel1, sel2 : std_logic_vector(0 to 4);
+    signal d_in, reg_out1, reg_out2 : std_logic_vector(0 to 31);
 begin
-    CoreRegistersInstance : entity work.CoreRegisters port map (clk => clk, l => l, sel => sel, d => d, q => q);
+    CoreRegistersInstance : entity work.CoreRegisters port map (clk => clk, l => l, sel1 => sel1, sel2 => sel2, d_in => d_in, reg_out1 => reg_out1, reg_out2 => reg_out2);
     ClockProcess : process
     begin
         while now < SIMULATION_LENGTH loop
@@ -27,30 +27,46 @@ begin
     CoreRegistersTestBench : process
     begin
         l <= '0';
-        d <= "00000000000000000000000000000000";
+        d_in <= "00000000000000000000000000000000";
+        sel2 <= "00000"; 
         for i in 0 to 31 loop
-            sel <= std_logic_vector(to_unsigned(i, 5));
+            sel1 <= std_logic_vector(to_unsigned(i, 5));
             wait for CLOCK_PERIOD;
-            assert (q = "UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU") report "testbench failed for CoreRegisters case 1" severity error;
+            assert (reg_out1 = "UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU") report "testbench failed for CoreRegisters case 1" severity error;
+            assert (reg_out2 = "UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU") report "testbench failed for CoreRegisters case 1" severity error;
         end loop;
         l <= '1';
         for i in 0 to 31 loop
-            sel <= std_logic_vector(to_unsigned(i, 5));
+            sel1 <= std_logic_vector(to_unsigned(i, 5));
             wait for CLOCK_PERIOD;
-            assert (q = "00000000000000000000000000000000") report "testbench failed for CoreRegisters case 2" severity error;
+            assert (reg_out1 = "00000000000000000000000000000000") report "testbench failed for CoreRegisters case 2" severity error;
+            assert (reg_out2 = "00000000000000000000000000000000") report "testbench failed for CoreRegisters case 2" severity error;
         end loop;
         for i in 0 to 31 loop
-            d <= std_logic_vector(to_unsigned(i, 32));
-            sel <= std_logic_vector(to_unsigned(i, 5));
+            d_in <= std_logic_vector(to_unsigned(i, 32));
+            sel1 <= std_logic_vector(to_unsigned(i, 5));
             wait for CLOCK_PERIOD;
-            assert (q = std_logic_vector(to_unsigned(i, 32))) report "testbench failed for CoreRegisters case 3" severity error;
+            assert (reg_out1 = std_logic_vector(to_unsigned(i, 32))) report "testbench failed for CoreRegisters case 3" severity error;
+            assert (reg_out2 = "00000000000000000000000000000000") report "testbench failed for CoreRegisters case 3" severity error;
         end loop;
         l <= '0';
         for i in 0 to 31 loop
-            sel <= std_logic_vector(to_unsigned(i, 5));
+            sel1 <= std_logic_vector(to_unsigned(i, 5));
             wait for CLOCK_PERIOD;
-            assert (q = std_logic_vector(to_unsigned(i, 32))) report "testbench failed for CoreRegisters case 4" severity error;
+            assert (reg_out1 = std_logic_vector(to_unsigned(i, 32))) report "testbench failed for CoreRegisters case 4" severity error;
+            assert (reg_out2 = "00000000000000000000000000000000") report "testbench failed for CoreRegisters case 4" severity error;
         end loop;
+        sel1 <= "01110";
+        sel2 <= "11100";
+        wait for CLOCK_PERIOD;
+        assert reg_out1 = "00000000000000000000000000001110" report "testbench failed for CoreRegisters case 5" severity error;
+        assert reg_out2 = "00000000000000000000000000011100" report "testbench failed for CoreRegisters case 5" severity error;
+        
+        sel1 <= "00010";
+        sel2 <= "11111";
+        wait for CLOCK_PERIOD;
+        assert reg_out1 = "00000000000000000000000000000010" report "testbench failed for CoreRegisters case 6" severity error;
+        assert reg_out2 = "00000000000000000000000000011111" report "testbench failed for CoreRegisters case 6" severity error;
 
         wait;
     end process;
